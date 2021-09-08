81e93795f06e075f9530b989c222d025dfa90860
Raian Satyro <raiansatyro45@gmail.com>
2021-09-08 20:13:01 -0300

    Tratamento do imput

--- main.py
+++ main.py
@@ -4,19 +4,32 @@ import json
 sair = False
 
 #abre arquivo jSON
-arquivo = open('./src/bd/bd.json', 'r')
+arquivo = open('./src/db/db.json', 'r')
 #Lê o arquivo jSON
 lista_cidades = json.loads(arquivo.read())
 #fecha o arquivo
 arquivo.close()
 
+
 while sair == False:
 
-    #Recebendo info da cidade
-    nome_cidade = input('Digite o nome da cidade: ')
-    populacao_cidade = int(input('Digite a população da cidade: '))
-    sigla_estado = input('Digite a sigla do estado: ')
-    nome_estado = input('Digite o nome do estado: ')
+#Recebendo info da cidade
+
+    nome_cidade = input('Digite o nome da cidade: ').title()
+
+    #Verifica se entra é numero inteiro
+    populacao_cidade = (input('Digite a população da cidade: '))
+    while not populacao_cidade.isnumeric():
+        print('Numero inválido')
+        populacao_cidade = (input('Digite a população da cidade: '))
+
+    #Trata para que seja digitado apenas letras com 2 caracteres
+    sigla_estado = input('Digite a sigla do estado: ').upper()
+    while not sigla_estado.isalpha() or len(sigla_estado) > 2:
+        print('Sigla inválida')
+        sigla_estado = input('Digite a sigla do estado: ').upper()
+
+    nome_estado = input('Digite o nome do estado: ').title()
 
     #instanciando a cidade
     uf = {'sigla': sigla_estado, 'nome': nome_estado}
@@ -45,7 +58,7 @@ while sair == False:
         sair = True
 
 #Abre o arquivo Json em modo escrita
-arquivo = open('./src/bd/bd.json', 'w')
+arquivo = open('./src/db/db.json', 'w')
 #Escreve no arquivo json a lista_cidades
 arquivo.write(json.dumps(lista_cidades))
 #Fecha o arquivo Json
